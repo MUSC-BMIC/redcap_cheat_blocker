b6c6840ae17b9a4c69741b96a55e8ffd7548713c
Andrew Cates <amcates@gmail.com>
2026-05-07 11:39:05 -0400

    oops, forgot this file

--- identify_duplicates.php
+++ identify_duplicates.php
@@ -3,6 +3,6 @@
 $duplicate_array = $module->check_for_duplicates($_GET);
 
 $content = json_encode($duplicate_array);
-RestUtility::sendResponse(200, $content);
+RestUtility::sendResponse(200, $content, 'json');
 
 ?>
